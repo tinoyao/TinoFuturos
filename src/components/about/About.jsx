6f79eacc9680506c68d54ab03e368c356efb49f8
tinoyao <dicienbre013@gmail.com>
2023-01-18 20:49:24 -0300

    se cambia logo nuevamente

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,13 @@ const About = () => {
     return(
         <div>
             <h1>View About</h1>
+            <h1>View About 2</h1>
+            <img
+                    alt=""
+                    src="/logo-original2.svg"
+                    width="800"
+                    height="400"
+                    />
         </div>
     )
 }
