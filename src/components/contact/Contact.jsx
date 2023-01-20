731cdde9a2a397b923d03e944505e6ae51d0e4af
tinoyao <dicienbre013@gmail.com>
2023-01-19 23:25:12 -0300

    como se cuanto trafico tiene mi pagina web para seo

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,8 @@ const Contact = () => {
     return(
         <div>
             <h1>View Contact</h1>
-            <h1>View Contact 2</h1>
+            <h2>como</h2>
+            <p>Hay varias formas de medir el tráfico de una página web hecha en React. Una forma es utilizar herramientas de análisis web como Google Analytics. Simplemente agregue el código de seguimiento de Google Analytics a su sitio web y podrá ver estadísticas sobre el tráfico, las páginas más populares, la fuente de tráfico, etc. Otro método podría ser usar una herramienta de seguimiento de tráfico web como Crazy Egg o Hotjar. Estas herramientas te darán una visión detallada de cómo los usuarios interactúan con tu sitio web.</p>
         </div>
     )
 }
