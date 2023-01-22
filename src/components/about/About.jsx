c2d260eea3faaa48436afe59bb2168fceaf46fdb
tinoyao <dicienbre013@gmail.com>
2023-01-21 21:29:23 -0300

    se mejora el nosotros, el contacto y los css

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,14 +1,71 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import { Box, Button, Container, Typography } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import s from './About.module.css';
+
+
 const About = () => {
     return(
-        <div>
-            <h1>View About</h1>
-            <h1>View About 2</h1>
-            <img
-                    alt=""
-                    src="/logo-original2.svg"
-                    width="800"
-                    height="400"
-                    />
+        <div className={s.containerAbout}>
+            <Box
+            component="main"
+            sx={{
+                alignItems: 'center',
+                display: 'flex',
+                flexGrow: 1,
+                minHeight: '100%'
+            }}
+            >
+                <Container maxWidth="md">
+                    <Box
+                    sx={{
+                        alignItems: 'center',
+                        display: 'flex',
+                        flexDirection: 'column'
+                    }}
+                    >
+                    <Typography
+                        align="center"
+                        color="blueviolet"
+                        variant="h3"
+                    >
+                        Este es el logo de anana tech solutions
+                    </Typography>
+                    {/* <Typography
+                        align="center"
+                        color="textPrimary"
+                        variant="subtitle2"
+                    >
+                        O intentaste una ruta sombría o viniste aquí por error.
+                        Sea lo que sea, intenta usar la navegación
+                    </Typography> */}
+                    <Box sx={{ textAlign: 'center' }}>
+                        <img
+                        alt="Under development"
+                        src="/logo-original2.svg"
+                        style={{
+                            marginTop: 50,
+                            display: 'inline-block',
+                            maxWidth: '100%',
+                            width: 560
+                        }}
+                        />
+                    </Box>
+                    <Link to='/' >
+                        <Button
+                            component="a"
+                            startIcon={(<ArrowBackIcon fontSize="small" />)}
+                            sx={{ mt: 3 }}
+                            variant="contained"
+                            color='secondary'
+                        >
+                            Volver
+                        </Button>
+                    </Link>
+                    </Box>
+                </Container>
+            </Box>
         </div>
     )
 }
--- src/components/error404/Error404.jsx
+++ src/components/error404/Error404.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import { Box, Button, Container, Typography } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import s from './Error404.module.css';
 
 
 function Error404() {
   return (
-    <div>
+    <div className={s.ContainerError}>
         <>
             <Box
             component="main"
@@ -17,54 +18,54 @@ function Error404() {
                 minHeight: '100%'
             }}
             >
-            <Container maxWidth="md">
-                <Box
-                sx={{
-                    alignItems: 'center',
-                    display: 'flex',
-                    flexDirection: 'column'
-                }}
-                >
-                <Typography
-                    align="center"
-                    color="blueviolet"
-                    variant="h3"
-                >
-                    404: La página que buscas no está aquí
-                </Typography>
-                <Typography
-                    align="center"
-                    color="textPrimary"
-                    variant="subtitle2"
-                >
-                    O intentaste una ruta sombría o viniste aquí por error.
-                    Sea lo que sea, intenta usar la navegación
-                </Typography>
-                <Box sx={{ textAlign: 'center' }}>
-                    <img
-                    alt="Under development"
-                    src="/errorPue.svg"
-                    style={{
-                        marginTop: 50,
-                        display: 'inline-block',
-                        maxWidth: '100%',
-                        width: 560
+                <Container maxWidth="md">
+                    <Box
+                    sx={{
+                        alignItems: 'center',
+                        display: 'flex',
+                        flexDirection: 'column'
                     }}
-                    />
-                </Box>
-                <Link to='/' >
-                    <Button
-                        component="a"
-                        startIcon={(<ArrowBackIcon fontSize="small" />)}
-                        sx={{ mt: 3 }}
-                        variant="contained"
-                        color='secondary'
                     >
-                        Volver
-                    </Button>
-                </Link>
-                </Box>
-            </Container>
+                    <Typography
+                        align="center"
+                        color="blueviolet"
+                        variant="h3"
+                    >
+                        404: La página que buscas no está aquí
+                    </Typography>
+                    <Typography
+                        align="center"
+                        color="textPrimary"
+                        variant="subtitle2"
+                    >
+                        O intentaste una ruta sombría o viniste aquí por error.
+                        Sea lo que sea, intenta usar la navegación
+                    </Typography>
+                    <Box sx={{ textAlign: 'center' }}>
+                        <img
+                        alt="Under development"
+                        src="/errorPue.svg"
+                        style={{
+                            marginTop: 50,
+                            display: 'inline-block',
+                            maxWidth: '100%',
+                            width: 560
+                        }}
+                        />
+                    </Box>
+                    <Link to='/' >
+                        <Button
+                            component="a"
+                            startIcon={(<ArrowBackIcon fontSize="small" />)}
+                            sx={{ mt: 3 }}
+                            variant="contained"
+                            color='secondary'
+                        >
+                            Volver
+                        </Button>
+                    </Link>
+                    </Box>
+                </Container>
             </Box>
         </>
     </div>
